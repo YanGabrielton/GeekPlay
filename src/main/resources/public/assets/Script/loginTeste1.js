555b9812f877cbf9263d3a1a9e701dfcac0c5b58
YanGabrielton <yangabrielton@gmail.com>
2025-05-27 22:04:08 -0300

    RegistroTeste

--- src/main/resources/public/assets/Script/loginTeste1.js
+++ src/main/resources/public/assets/Script/loginTeste1.js
@@ -27,23 +27,38 @@ async function fazerLogin() {
                 senha: document.getElementById("password").value
             })
         });
-
-       
-
         const data = await response.json(); 
-    
         if (data.success) {
             alert("Login bem-sucedido! Bem-vindo, " + data.usuario.nome);
             // Redireciona ou faz algo após login
             window.location.href = "/src/main/resources/public/index.html";
-
-
-
-        
         } else {
             alert("Erro: " + data.message); // Usa message em vez de error
         }
 
+    } catch (error) {
+        alert("Erro na conexão com o servidor");
+        console.error("Erro:", error);
+    }
+}
+async function fazerRegistro() {
+    try {
+        const response = await fetch("http://localhost:7070/usuarios", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                nome: document.getElementById("nome").value,
+                email: document.getElementById("email1").value,
+                senha: document.getElementById("password1").value
+            })
+        });
+        const data = await response.json();
+        if (data.success) {
+            alert("Registro bem-sucedido! Você pode fazer login agora.");
+            container.classList.remove("active");
+        } else {
+            alert("Erro: " + data.message);
+        }
     } catch (error) {
         alert("Erro na conexão com o servidor");
         console.error("Erro:", error);
